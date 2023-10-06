3ec676bb3ad763dd59066fc43e9e13de680dc219
david-peyre <david.peyre@laplateforme.io>
2023-10-06 12:03:50 +0200

    fix accessrights

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -4,14 +4,13 @@ FICHIER_CSV="/home/david/DocsLPF/shell.exe/Job09/Shell_Userlist.csv"
 
 while IFS=',' read -r id prenom nom mdp role
 do
-
-if [ "$prenom" != "Prénom" ]; then
-if [ "$role" == "Admin" ]; then
-
-     useradd -m -G sudo "$prenom"
-     echo "L'utilisateur $prenom a été ajouté avec permissions - super utilisateur -"
-else
-     useradd -m "$prenom"
-     echo "L'utilisateur - $prenom - a été ajouté avec des permissions - normales-"
-fi
+    if [ "$prenom" != "Prenom" ]; then
+        if [ "$role" == "Admin" ]; then
+            useradd -m -G sudo "$prenom"
+            echo "L'utilisateur $prenom a été ajouté avec des permissions - super utilisateur -"
+        else
+            useradd -m "$prenom"
+            echo "L'utilisateur - $prenom - a été ajouté avec des permissions - normales-"
+        fi
+    fi
 done < "$FICHIER_CSV"
